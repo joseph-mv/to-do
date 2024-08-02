7eb43deb24e3e273c7e2731d105127b99ba85117
josephmv43 <josephmv43@gmail.com>
2024-08-02 12:48:51 +0530

    for versal deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [task, setTask] = useState('')
+  const [description,setDescription]=useState('')
+  const [dueDate,setDueDate]=useState('')
+  const [toDos, setToDOs] = useState([])
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const today = new Date();
+  const day = days[today.getDay()]; // Get day index (0-6) and use it to access the day name
+
+ 
   return (
     <div className="app">
       <div className="mainHeading">
@@ -9,26 +19,66 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's {day} 🌝 ☕ </h2>
       </div>
-      <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
-      </div>
+    
+      <div className="input" >
+        
+        <input className='task' type="text" value={task} onChange={(e) => { setTask(e.target.value) }} placeholder="🖊️ Task..." /> 
+        <input className='description' value={description} onChange={(e) => { setDescription(e.target.value) }} type="text"  placeholder="Enter a Description" /> 
+       <div className='dueDate'>
+       <p>Due Date:  </p>
+       <input type='date' id='myDate' min={new Date().toISOString().slice(0, 10)} value={dueDate} onChange={(e) => { setDueDate(e.target.value) }}  />
+        <i onClick={() => setToDOs([...toDos, { text: task, description,dueDate, status: false, id: Date.now() }])} className="fas fa-plus"></i>
+      
+       </div>
+     </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {toDos.map(value => {
+          console.log(value)
+          return (
+            <div className="todo">
+              <div className="left">
+                <input onChange={(e) => {
+                  var ticked = toDos.filter((obj) => {
+                    if (obj.id === value.id) {
+                      obj.status = e.target.checked
+                    }
+                    return obj
+                  }
+                  )
+                  // console.log(ticked)
+                  setToDOs(ticked)
+                }} type="checkbox" name="" id="" />
+                <div className='toDoDetails'>
+                <p><span>Task:</span>{value.text}</p>
+                <p><span>Description:</span>{value.description}</p>
+                <p><span>Due Date:</span>{value.dueDate}</p>
+                </div>
+               
+              </div>
+              <div className="right">
+                <i className="fas fa-times" onClick={(e) => { setToDOs(toDos.filter((obj) => (obj.id !== value.id))) }}></i>
+              </div>
+            </div>
+          )
+        })}
+
+        {toDos.map((obj) => {
+          if (obj.status === true) {
+            return (<p>{obj.text}</p>)
+          }
+          else {
+            return (<p></p>)
+          }
+        })}
+
+
       </div>
     </div>
   );
 }
+
 
 export default App;
 
